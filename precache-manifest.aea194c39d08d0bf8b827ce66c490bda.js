4cbe4643365743a874f6061d6d7b733b7007f451
Artem Ipatov <artemipatov111@gmail.com>
2020-01-20 17:06:43 +0200

    JS course work

---
+++ precache-manifest.aea194c39d08d0bf8b827ce66c490bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ff44efb6c7fbaf484aafb571e6bbb88",
+    "url": "/static/media/header1.9ff44efb.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba485d9bbd919bf90b23",
+    "url": "/static/js/main.ba485d9b.chunk.js"
+  },
+  {
+    "revision": "7d0a36a0197e5d0febd8",
+    "url": "/static/js/1.7d0a36a0.chunk.js"
+  },
+  {
+    "revision": "ba485d9bbd919bf90b23",
+    "url": "/static/css/main.148d7567.chunk.css"
+  },
+  {
+    "revision": "8fe347a68813be7b8f58a0f951976a61",
+    "url": "/index.html"
+  }
+];
